386bba258f2cacba520fc57531a0ba4cf017fd3b
maaretp <maaret@iki.fi>
2025-04-07 10:35:48 +0300

    Change tests to run against ELM version

--- tests/demo-todo-app.spec.js
+++ tests/demo-todo-app.spec.js
@@ -2,7 +2,7 @@
 const { test, expect } = require('@playwright/test');
 
 test.beforeEach(async ({ page }) => {
-  await page.goto('https://demo.playwright.dev/todomvc');
+  await page.goto('https://todomvc.com/examples/elm/');
 });
 
 const TODO_ITEMS = [
