2fd02f4e1a4b18e03f6d280ca802f8c4cb41a48c
maaretp <maaret@iki.fi>
2025-04-07 10:54:43 +0300

    Revert to playwright demo site

--- tests/demo-todo-app.spec.js
+++ tests/demo-todo-app.spec.js
@@ -2,7 +2,7 @@
 const { test, expect } = require('@playwright/test');
 
 test.beforeEach(async ({ page }) => {
-  await page.goto('https://todomvc.com/examples/elm/');
+  await page.goto('https://demo.playwright.dev/todomvc');
 });
 
 const TODO_ITEMS = [
